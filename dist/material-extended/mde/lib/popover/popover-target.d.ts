2a28b604ba3a0cf8afe28e3750856313e9449056
fpfcarvalho <fpfcarvalho@gmail.com>
2022-08-29 12:20:33 +0100

    Remove dist folder

--- dist/material-extended/mde/lib/popover/popover-target.d.ts
+++
@@ -1,8 +0,0 @@
-import { ElementRef } from '@angular/core';
-import * as i0 from "@angular/core";
-export declare class MdePopoverTarget {
-    _elementRef: ElementRef;
-    constructor(_elementRef: ElementRef);
-    static ɵfac: i0.ɵɵFactoryDeclaration<MdePopoverTarget, never>;
-    static ɵdir: i0.ɵɵDirectiveDeclaration<MdePopoverTarget, "mde-popover-target, [mdePopoverTarget]", ["mdePopoverTarget"], {}, {}, never>;
-}
